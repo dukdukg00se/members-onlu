9f3ca630de74c70b815ca2918a4ea85f899c8573
Will H <will.wkhu@gmail.com>
2024-06-14 23:14:09 -0700

    Work on page routes

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const index_controller = require('../controllers/indexController');
 
-// router.get('/', (req, res, next) => {
-//   res.render('index', {
-//     title: 'Members Only',
-//   });
-// });
-
 router.get('/', index_controller.index);
+
+router.get('/login', index_controller.login_get);
+
+router.post('/login', index_controller.login_post);
+
+router.get('/signup', index_controller.signup_get);
+
+router.post('/signup', index_controller.signup_post);
 
 module.exports = router;
