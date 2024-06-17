db22c24cc9808e7ca1bff426a6ba996c35df9522
Will H <will.wkhu@gmail.com>
2024-06-17 15:15:54 -0700

    Add auth module to new message route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 const index_controller = require('../controllers/indexController');
+const isAuth = require('./authMidware').isAuth;
 
 router.get('/', index_controller.index);
 
@@ -12,5 +12,7 @@ router.post('/login', index_controller.login_post);
 router.get('/signup', index_controller.signup_get);
 
 router.post('/signup', index_controller.signup_post);
+
+router.get('/create', isAuth, index_controller.message_create_get);
 
 module.exports = router;
